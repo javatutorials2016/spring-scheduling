176f5e49dc254a53856ca03e42abb58bedc980bf
javapedia.net <javatutorials2016@gmail.com>
2020-03-28 13:53:18 -0400

    adding fixedRate method

--- src/main/java/net/javapedia/springscheduling/services/ScheduledService.java
+++ src/main/java/net/javapedia/springscheduling/services/ScheduledService.java
@@ -6,11 +6,17 @@ import org.springframework.stereotype.Component;
 @Component
 public class ScheduledService {
 
-    int i=0;
-    @Scheduled(fixedDelay = 2000)
+    private int i = 0;
+
+    @Scheduled(fixedDelay = 2000) //in ms
     public void myFixedDelayScheduledMethod() {
         i++;
-        System.out.println ("Running scheduled Task, iteration :"+ i);
+        System.out.println("Running scheduled Task, iteration :" + i + " Thread name: " + Thread.currentThread().getName());
     }
 
+
+    @Scheduled(fixedRate = 4000) //in ms
+    public void myFixedRateScheduledMethod() {
+        System.out.println("Running fixedRate Task, Thread name: " + Thread.currentThread().getName());
+    }
 }
